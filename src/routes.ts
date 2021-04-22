afe8f4c748acadaa706e00be149e070e39a30ce1
João Vitor Mariano da Silva <69213358+Jvnb401@users.noreply.github.com>
2021-04-22 13:15:24 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { SettingsController } from "./controllers/SettingsController";
-import { UsersController } from "./controllers/UsersController"
+import { UsersController } from "./controllers/UsersController";
+import { MessagesController } from "./controllers/MessagesControler";
 
 const routes = Router();
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messageController = new MessagesController();
 
-routes.post("/setting", settingsController.create);
+routes.post("/settings", settingsController.create);
 routes.post("/users", usersController.create);
+routes.post("/messages", messageController.create);
+routes.get("/messages/:id", messageController.showByUser);
 
 export { routes };
