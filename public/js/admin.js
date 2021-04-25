6853a9f69c50e34e3cf39e811c1d80a36a3cd6f9
João Vitor Mariano da Silva <69213358+Jvnb401@users.noreply.github.com>
2021-04-24 22:37:17 -0300

    Add files via upload

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,7 @@
-const Socket = io();
+const socket = io();
 let connectionsUsers = [];
 
-Socket.on("admin_list_all_users", connections => {
+socket.on("admin_list_all_users", connections => {
     connectionsUsers = connections;
     document.getElementById("list_users").innerHTML = "";
 
@@ -10,7 +10,7 @@ Socket.on("admin_list_all_users", connections => {
     connections.forEach(connection => {
         const rendered = Mustache.render(template, {
             email: connection.user.email,
-            id: connection.Socket_id,
+            id: connection.socket_id,
         });
 
         document.getElementById("list_users").innerHTML += rendered;
@@ -21,9 +21,6 @@ function call(id) {
     const connection = connectionsUsers.find(
         (connection) => connection.socket_id === id
     );
-
-    console.log(connectionsUsers);
-
     const template = document.getElementById("admin_template").innerHTML;
     const rendered = Mustache.render(template, {
         email: connection.user.email,
@@ -31,4 +28,51 @@ function call(id) {
     });
 
     document.getElementById("supports").innerHTML += rendered;
+
+    const params = { user_id: connection.user_id, }
+
+    socket.emit("admin_list_messages_by_user", params, messages => {
+        const divMessages = document.getElementById(`allMessages${connection.user_id}`);
+
+        messages.forEach((message) => {
+            const createDiv = document.createElement("div");
+
+            if (message.admin_id === null) {
+                createDiv.className = "admin_message_client";
+
+                createDiv.innerHTML = `<span>${connection.user.email} <br> - ${message.text}</span>`;
+                createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+            } else {
+                createDiv.className = "admin_message_admin";
+
+                createDiv.innerHTML = `atendente: <span>${message.text}</span>`;
+                createDiv.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+            }
+            divMessages.appendChild(createDiv);
+        });
+
+    });
+}
+
+function sendMessage(id) {
+    const text = document.getElementById(`send_message_${id}`);
+
+    const params = {
+        text: text.value,
+        user_id: id,
+    };
+
+    socket.emit("admin_send_message", params);
+
+    const divMessages = document.getElementById(`allMessages${id}`);
+    const createDiv = document.createElement("div");
+    createDiv.className = "admin_message_admin";
+
+    createDiv.innerHTML = `atendente: <span>${params.text}</span>`;
+    createDiv.innerHTML += `<span class="admin_date">${dayjs(
+    ).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+
+    divMessages.appendChild(createDiv);
+
+    text.value = "";
 }
